c001165b674ac04a1998e9ae0cd670368f125720
Alina Eskova <pulgans94@gmail.com>
2020-03-18 20:40:49 +0300

    Updated index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  let getSum = function(a, b) {
+    return a + b;
+    }
+  return ( array.reduce(getSum) ) / array.length;
 }
+
